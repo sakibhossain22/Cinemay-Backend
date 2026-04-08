c5b9f5bc6fd67b0128d870bbfdb8ca67fb298ca2
Md Sakib Mia <sakib01181@gmail.com>
2026-04-08 21:07:56 +0600

    auth updated

--- src/auth/auth.routes.ts
+++ src/auth/auth.routes.ts
@@ -7,6 +7,7 @@ const router = Router();
 
 router.post("/register", authController.register)
 router.post("/login", authController.login)
+router.post("/google-login", authController.googleLogin)
 
 router.post("/forgot-password", authController.forgotPassword);
 router.post("/reset-password", authController.resetPassword);
